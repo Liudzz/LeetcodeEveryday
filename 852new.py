68489fb9d80ae5aae6970c4e1fa2b6b2b4461c1a
Liudzz <2028402011@qq.com>
2020-12-07 11:47:06 +0900

    看题解后尝试用max和index或二分法

---
+++ 852new.py
@@ -0,0 +1,12 @@
+class Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        #return arr.index(max(arr)) #方法1 36ms 93.41%
+        min, max = 0, len(arr) - 1
+        while (min < max):
+            mid = min + int((max - min) / 2)
+            if arr[mid] > arr[mid - 1] and arr[mid] > arr[mid + 1]:
+                return mid
+            elif arr[mid] > arr[mid - 1] and arr[mid] < arr[mid + 1]:
+                min = mid
+            elif arr[mid] < arr[mid - 1] and arr[mid] > arr[mid + 1]:
+                max = mid
