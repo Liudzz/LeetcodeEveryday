93df815c6c20121e86f14db2148c8498da5db0e2
Liudzz <2028402011@qq.com>
2020-12-11 17:54:29 +0800

    9*

---
+++ 9/9.回文数.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        x = str(x)
+        for i in range(int(len(x)/2)):
+            if x[i] != x[-(i+1)]:
+                return False
+        return True
