f0f7728453fccd3cfc2f332add32366aae15db7a
Liudzz <2028402011@qq.com>
2020-12-11 17:49:16 +0800

    del root files

--- 1.两数之和(hashtable).py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hashmap = dict()
-        for i in range(len(nums)):
-            if (target-nums[i]) in hashmap.keys():
-                if hashmap[target-nums[i]]!=i:
-                    return [i,hashmap[target-nums[i]]]
-            else:
-                hashmap[nums[i]]=i
--- 9/9.回文数.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def isPalindrome(self, x: int) -> bool:
-        x = str(x)
-        for i in range(int(len(x)/2)):
-            if x[i] != x[-(i+1)]:
-                return False
-        return True
